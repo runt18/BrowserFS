207e4c0f3746da7d830bbca3728e7355271b50b3
Giles Lavelle <giles.lavelle@gmail.com>
2013-08-30 22:14:02 +0100

    use new constructor signature

--- test/db-setup.js
+++ test/db-setup.js
@@ -1,6 +1,6 @@
 if(BrowserFS.FileSystem.Dropbox.isAvailable()){
     // Pass true to enable testing mode
-    dbfs = new BrowserFS.FileSystem.Dropbox(true);
+    dbfs = new BrowserFS.FileSystem.Dropbox(null, true);
     backends.push(dbfs);
     dbfs.empty(function(){
         generateAllTests();
