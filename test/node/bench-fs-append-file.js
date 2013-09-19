57115fcd81bd762083c5337800f2a45aae35df2b
Giles Lavelle <giles.lavelle@gmail.com>
2013-09-19 18:16:05 +0100

    filled out append test

--- test/node/bench-fs-append-file.js
+++ test/node/bench-fs-append-file.js
@@ -1,17 +1,42 @@
+var randomString = function(length){
+    var chars = [];
+    for(var i = 0; i < length; i++){
+        chars.push(String.fromCharCode(Math.floor(Math.random() * 100)));
+    }
+    return chars.join('');
+};
+
 this.tests.bench_append = function(){
     var rootFS = fs.getRootFS();
     if (rootFS.isReadOnly()) return;
 
+    var numStrings = 100;
+    var length = 100;
+    var strings = [];
+
+    for(var i = 0; i < numStrings; i++){
+        strings.push(randomString(length));
+    }
+
     var start = Date.now();
 
-    fs.appendFile('file.txt', "new data", function(err){
-        if(err){
-            throw new Error('Benchmark: append file failed.');
-        }
-        else {
-            var end = Date.now();
-            var diff = end - start;
-            console.log('Append ran in ' + diff + ' ms.');
-        }
-    });
+    var append = function(data, cb){
+        fs.appendFile('append-bench.txt', data, function(err){
+            if(err){
+                throw new Error('Benchmark: append file failed.');
+                cb(err);
+            }
+            else {
+                cb(null);
+            }
+        });
+    };
+
+    var finished = function(err){
+        var end = Date.now();
+        var diff = end - start;
+        console.log('Appended ' + numStrings + ' strings in ' + diff + ' ms.');
+    };
+
+    async.each(strings, append, finished);
 };
