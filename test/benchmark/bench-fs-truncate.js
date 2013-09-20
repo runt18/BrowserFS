be73e6d6b969675264af12bf2b7a1ce613d46e0a
Giles Lavelle <giles.lavelle@gmail.com>
2013-09-20 21:55:10 +0100

    fixed running multiple trials in the benchmark

--- test/benchmark/bench-fs-truncate.js
+++ test/benchmark/bench-fs-truncate.js
@@ -15,7 +15,7 @@ this.benchmarks.bench_truncate = function(){
   if (rootFS.isReadOnly()) return;
 
   var times = [];
-  var trials = 2;
+  var trials = 10;
   var lengths = [];
   var iterations = 100;
   var i = iterations;
@@ -25,11 +25,28 @@ this.benchmarks.bench_truncate = function(){
 
   var startData = new Array(100).join('a');
 
+  var finished = function(err){
+    if(err){
+      fail(err);
+    }
+    else {
+      var end = Date.now();
+      var diff = end - start;
+      times.push(diff);
+      if(times.length === trials){
+        console.log(rootFS.getName() + ' backend truncated a file ' + iterations + ' times in ' + mean(times) + ' ms.');
+      }
+    }
+  };
+
   var fail = function(err){
     throw new Error('Benchmark: truncate file failed: ' + err);
   };
 
-  for(var i = 0; i < trials; i++){
+  var i = 0;
+  var start;
+
+  var runLoop = function(){
     var path = 'truncate' + i + '.txt';
     fs.writeFile(path, startData, function(err){
       if (err) {
@@ -48,22 +65,12 @@ this.benchmarks.bench_truncate = function(){
           });
         };
 
-        var finished = function(err){
-          if(err){
-            fail(err);
-          }
-          else {
-            var end = Date.now();
-            var diff = end - start;
-            times.push(diff);
-            if(times.length === trials){
-              console.log(rootFS.getName() + ' backend truncated a file ' + iterations + ' times in ' + mean(times) + ' ms.');
-            }
-          }
-        };
-
-        var start = Date.now();
+        start = Date.now();
         async.eachSeries(lengths, truncate, finished);
+        i++;
+        if(i < trials){
+          runLoop();
+        }
       }
     });
   }
